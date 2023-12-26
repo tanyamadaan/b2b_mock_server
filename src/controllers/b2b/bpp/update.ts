a2f406bd8d7aa4e5558c82b3c9646dba3594682d
pratek-0328 <prateek21181@iiitd.ac.in>
2023-12-26 17:07:01 +0530

    changed the status and update controller

--- src/controllers/b2b/bpp/update.ts
+++ src/controllers/b2b/bpp/update.ts
@@ -1,13 +1,49 @@
 import { Request, Response } from "express";
+import { onUpdateFulfillments } from "../../../lib/examples";
+import { onUpdatePrepaid } from "../../../lib/examples";
 
-export const updateController = (req: Request, res: Response) => {
-  const context = req.body.context;
+export const updateFulfillmentController = (req: Request, res: Response) => {
+  var ts = new Date(req.body.context.timestamp);
+	ts.setSeconds(ts.getSeconds() + 1);
+  const context = {
+		...req.body.context,
+		action: "on_update",
+		timeStamp: ts.toISOString(),
+	};
   return res.json({
-    context,
-    message: {
-      ack: {
-        status: "ACK",
-      },
-    },
-  });
+		sync: {
+			message: {
+				ack: {
+					status: "ACK",
+				},
+			},
+		},
+		async: {
+			context,
+			message: onUpdateFulfillments.message,
+		},
+	});
+};
+
+export const updatePrepaidController = (req: Request, res: Response) => {
+  var ts = new Date(req.body.context.timestamp);
+	ts.setSeconds(ts.getSeconds() + 1);
+  const context = {
+		...req.body.context,
+		action: "on_update",
+		timeStamp: ts.toISOString(),
+	};
+  return res.json({
+		sync: {
+			message: {
+				ack: {
+					status: "ACK",
+				},
+			},
+		},
+		async: {
+			context,
+			message: onUpdatePrepaid.message,
+		},
+	});
 };
