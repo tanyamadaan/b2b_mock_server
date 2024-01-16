63917366874dcdf748f613640366497567087035
Abhik Banerjee <abhik.banerjee@thewitslab.com>
2024-01-16 13:11:02 +0530

    Scripts for beck-core related yaml for swagger generated.

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import path from "path";
 const app: Express = express();
 const port = 3000;
 
-const file = fs.readFileSync(path.join(__dirname, "./openapi/openapi.yaml"), "utf8");
+const file = fs.readFileSync(path.join(__dirname, "./openapi/build/swagger.yaml"), "utf8");
 const swaggerDocument = YAML.parse(file);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
