7326485f41ab628ab76a7d0625ec6dfbd2e66f5a
Abhik Banerjee <abhik.banerjee@thewitslab.com>
2024-01-18 16:36:44 +0530

    on_select response defined.

--- src/controllers/b2b/bpp/select.ts
+++ src/controllers/b2b/bpp/select.ts
@@ -3,11 +3,81 @@ import { onSelectDomestic } from "../../../lib/examples";
 import { ACTIONS, responseBuilder } from "../../../lib/utils";
 
 export const selectController = (req: Request, res: Response) => {
+	const { context, message } = req.body;
+	const { ttl, ...provider } = message.order.provider;
+	var breakup: any[] = [];
+	const onFulfillment = onSelectDomestic.message.order.quote.breakup.filter(
+		(each) => each["@ondc/org/item_id"] === "F1"
+	);
+	const onItem = onSelectDomestic.message.order.quote.breakup.filter(
+		(each) =>
+			each["@ondc/org/item_id"] === "I1" &&
+			each["@ondc/org/title_type"] !== "item"
+	);
+	message.order.items.forEach((item: any) => {
+		breakup = [
+			...breakup,
+			...onItem,
+			{
+				"@ondc/org/item_id": item.id,
+				"@ondc/org/item_quantity": {
+					count: item.quantity.selected.count,
+				},
+				title: "Product Name Here",
+				"@ondc/org/title_type": "item",
+				price: {
+					currency: "INR",
+					value: (item.quantity.selected.count * 250).toString(),
+				},
+				item: {
+					price: {
+						currency: "INR",
+						value: "250",
+					},
+				},
+			},
+		];
+		item.fulfillment_ids.forEach((eachId: string) => {
+			breakup = [
+				...breakup,
+				...onFulfillment.map(each => ({...each, "@ondc/org/item_id": eachId}))
+			]
+		});
+	});
+
+	var responseMessage = {
+		order: {
+			provider,
+			payments: message.order.payments.map(({ type }: { type: string }) => ({
+				type,
+				collected_by: "BPP",
+			})),
+			items: message.items.map(
+				({
+					location_ids,
+					...remaining
+				}: {
+					location_ids: any;
+					remaining: any;
+				}) => ({
+					remaining,
+				})
+			),
+			fulfillments: message.fulfillments,
+			quote: {
+				breakup,
+				price: {
+					currency: "INR",
+					value: (53_600 * message.order.items.length).toString()
+				}
+			}
+		},
+	};
 	return responseBuilder(
 		res,
-		req.body.context,
-		onSelectDomestic.message,
-		req.body.context.bap_uri,
+		context,
+		responseMessage,
+		context.bap_uri,
 		`on_${ACTIONS.select}`
 	);
 };
