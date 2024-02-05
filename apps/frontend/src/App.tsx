912f19867efdd00845d2b3d0216a14d170cbabfc
Abhik Banerjee <abhik.banerjee@thewitslab.com>
2024-02-05 11:47:10 +0530

    Added Frontend Baseline.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,35 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Typography from "@mui/material/Typography";
+import { CustomDrawer } from "./components";
+import Paper from "@mui/material/Paper";
+import Fade from "@mui/material/Fade";
+import Input from "@mui/joy/Input";
+import Textarea from "@mui/joy/Textarea";
+import {
+	Box,
+	Button,
+	FormControl,
+	FormHelperText,
+	Stack,
+	Switch,
+} from "@mui/joy";
+import { useState } from "react";
+import { InfoOutlined } from "@mui/icons-material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [authHeader, setAuthHeader] = useState<string>();
+	const [log, setLog] = useState<string>();
+	const [action, setAction] = useState<string>();
+	const [logError, setLogError] = useState(false);
+	const [mockerNP, setMockerNP] = useState<boolean>(0); // false-> Buyer/BAP; true -> Seller/BPP
+	const handleLogChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		try {
+			setLog(e.target.value);
+			const parsedLog = JSON.parse(e.target.value);
+			if (!parsedLog.context!.action) setLogError(true);
+			setAction(parsedLog.context.action);
+			setLogError(false);
+		} catch (error) {
+			console.log("Error Occurred in LOG", error);
+			setLogError(true);
+			setAction(undefined);
+		}
+	};
+	const handleSubmit = () => {
+		console.log("Form Values", authHeader, log);
+	};
+	return (
+		<>
+			<CustomDrawer>
+				{" "}
+				<Fade in={true} timeout={2000}>
+					<Paper
+						sx={{
+							mt: 4,
+							p: 2,
+						}}
+					>
+						<Stack spacing={2} justifyContent="center" alignItems="center">
+							<Typography variant="h3">ONDC Mock Server</Typography>
+							<Switch
+								color={mockerNP ? "warning" : "danger"}
+								slotProps={{ input: { "aria-label": "dark mode" } }}
+								startDecorator={<Typography>Buyer</Typography>}
+								endDecorator={<Typography>Seller</Typography>}
+								checked={mockerNP}
+								onChange={() => setMockerNP((prev) => !prev)}
+							/>
+							<Input
+								fullWidth
+								placeholder="Enter Your Auth Header..."
+								value={authHeader}
+								onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+									setAuthHeader(e.target.value)
+								}
+							/>
+							<FormControl error={logError} sx={{ width: "100%" }}>
+								<Textarea
+									minRows={5}
+									sx={{ width: "100%" }}
+									placeholder="Enter Your Log..."
+									value={log}
+									onChange={handleLogChange}
+								/>
+								{logError && (
+									<FormHelperText>
+										<InfoOutlined />
+										Opps! The log seems to be invalid.
+									</FormHelperText>
+								)}
+							</FormControl>
+							{action && (
+								<Box
+									sx={{
+										width: "100%",
+										display: "flex",
+										justifyContent: "flex-start",
+										alignItems: "baseline",
+									}}
+								>
+									<Typography variant="body1" mr={2}>
+										Detected Action:
+									</Typography>
+									<Typography color="text.secondary" variant="body2">
+										{action}
+									</Typography>
+								</Box>
+							)}
+							<Button variant="soft" onClick={handleSubmit}>
+								Submit
+							</Button>
+						</Stack>
+					</Paper>
+				</Fade>
+			</CustomDrawer>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ apps/frontend/src/components/CustomDrawer.tsx
@@ -0,0 +1,117 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import Grow from "@mui/material/Grow";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Toolbar from "@mui/material/Toolbar";
+import useTheme from "@mui/material/styles/useTheme";
+import React from "react";
+
+const drawerWidth = 240;
+
+type CustomDrawerProps = {
+	children: React.ReactNode;
+};
+
+export const CustomDrawer = ({ children }: CustomDrawerProps) => {
+  const theme = useTheme();
+	const [mobileOpen, setMobileOpen] = React.useState(false);
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [_isClosing, setIsClosing] = React.useState(false);
+
+	const handleDrawerClose = () => {
+		setIsClosing(true);
+		setMobileOpen(false);
+	};
+
+	const handleDrawerTransitionEnd = () => {
+		setIsClosing(false);
+	};
+
+	// const handleDrawerToggle = () => {
+	// 	if (!isClosing) {
+	// 		setMobileOpen(!mobileOpen);
+	// 	}
+	// };
+	const drawer = (
+		<div>
+			<Toolbar />
+			<Divider />
+			<List>
+				{["Mock Server", "Swagger Defination"].map((text, index) => (
+          <Grow in={true} timeout={1000}>
+					<ListItem key={index} disablePadding >
+						<ListItemButton>
+							<ListItemText primary={text} />
+						</ListItemButton>
+					</ListItem>
+          </Grow>
+				))}
+			</List>
+			<Divider />
+		</div>
+	);
+
+	return (
+		<Box sx={{ display: "flex" }}>
+			<Box
+				component="nav"
+				sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+			>
+				<Drawer
+					variant="temporary"
+					open={mobileOpen}
+					onTransitionEnd={handleDrawerTransitionEnd}
+					onClose={handleDrawerClose}
+					ModalProps={{
+						keepMounted: true, // Better open performance on mobile.
+					}}
+					sx={{
+						display: { xs: "block", sm: "none" },
+						"& .MuiDrawer-paper": {
+							boxSizing: "border-box",
+							width: drawerWidth,
+						},
+					}}
+				>
+					{drawer}
+				</Drawer>
+				<Drawer
+					variant="permanent"
+          PaperProps={{
+            sx: {
+              backgroundColor: theme.palette.primary.dark,
+              color: theme.palette.primary.contrastText
+            }
+          }}
+					sx={{
+						display: { xs: "none", sm: "block" },
+						"& .MuiDrawer-paper": {
+							boxSizing: "border-box",
+							width: drawerWidth,
+						},
+					}}
+					open
+				>
+					{drawer}
+				</Drawer>
+			</Box>
+			<Container
+				component="main"
+				sx={{
+					flexGrow: 1,
+					// p: 3,
+          height: "100vh",
+					width: { sm: `calc(100% - ${drawerWidth}px)` },
+          backgroundColor: theme.palette.grey[100]
+				}}
+			>
+				{children}
+			</Container>
+		</Box>
+	);
+};
