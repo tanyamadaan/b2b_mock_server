9f04a2e50d3aee8ad987f140088a70a9998713c4
dinesh k <dinesh.k@thewitslab.com>
2024-02-15 16:49:53 +0530

    1. added scenario as query in bap endpoints 2. updated swagger as well for above

--- apps/backend/src/controllers/b2b/bap/onSelect.ts
+++ apps/backend/src/controllers/b2b/bap/onSelect.ts
@@ -1,8 +1,38 @@
 import { Request, Response } from "express";
-import { initDomestic } from "../../../lib/examples";
+import { initDomestic, initDomesticBPPPayment, initDomesticNonRFQ, initExports } from "../../../lib/examples";
 import { ACTIONS, responseBuilder } from "../../../lib/utils";
 
 export const onSelectController = (req: Request, res: Response) => {
+	const { scenario } = req.query
+	switch (scenario) {
+		case 'domestic':
+			onSelectDomesticController(req, res)
+			break;
+		case 'domestic-non-rfq':
+			onSelectDomesticNonRfqController(req, res)
+			break;
+		case 'exports':
+			onSelectExportsController(req, res)
+			break;
+		case 'domestic-bpp-payment':
+			onSelectDomesticBPPPaymentController(req, res)
+			break;
+		default:
+			res.status(404).json({
+				message: {
+					ack: {
+						status: "NACK",
+					},
+				},
+				error: {
+					message: "Invalid scenario",
+				},
+			});
+			break;
+	}
+};
+
+export const onSelectDomesticController = (req: Request, res: Response) => {
 	const {
 		context,
 		message: {
@@ -25,7 +55,37 @@ export const onSelectController = (req: Request, res: Response) => {
 		res,
 		context,
 		responseMessage,
-		`${context.bap_uri}/${ACTIONS.init}`,
+		`${context.bpp_uri}/${ACTIONS.init}`,
 		ACTIONS.init
+	);
+};
+
+export const onSelectDomesticNonRfqController = (req: Request, res: Response) => {
+	return responseBuilder(
+		res,
+		req.body.context,
+		initDomesticNonRFQ.message,
+		req.body.context.bpp_uri,
+		`${ACTIONS.init}`
+	);
+};
+
+export const onSelectExportsController = (req: Request, res: Response) => {
+	return responseBuilder(
+		res,
+		req.body.context,
+		initExports.message,
+		req.body.context.bpp_uri,
+		`${ACTIONS.init}`
+	);
+};
+
+export const onSelectDomesticBPPPaymentController = (req: Request, res: Response) => {
+	return responseBuilder(
+		res,
+		req.body.context,
+		initDomesticBPPPayment.message,
+		req.body.context.bpp_uri,
+		`${ACTIONS.init}`
 	);
 };
