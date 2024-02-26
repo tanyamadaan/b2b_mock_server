dd39834fbf3635e6c74156b91b33e1880ddd4b7c
Abhik Banerjee <abhik.banerjee@thewitslab.com>
2024-02-26 11:50:32 +0530

    Completed onInit.

--- apps/backend/src/controllers/services/bap/onInit.ts
+++ apps/backend/src/controllers/services/bap/onInit.ts
@@ -1,19 +1,17 @@
 import { Request, Response } from "express";
-import { v4 as uuidv4 } from "uuid";
-import { confirmExports, confirmNonRFQ } from "../../../lib/examples";
-import { ACTIONS, responseBuilder } from "../../../lib/utils";
+import { ACTIONS, SERVICES_EXAMPLES_PATH, responseBuilder } from "../../../lib/utils";
+import fs from "fs";
+import path from "path";
+import YAML from "yaml";
 
 export const onInitController = (req: Request, res: Response) => {
 	const { scenario } = req.query
 	switch (scenario) {
-		case 'rfq':
-			onInitDomesticController(req, res)
+		case 'consultation':
+			onInitConsultationController(req, res)
 			break;
-		case 'non-rfq':
-			onInitDomesticNonRfqController(req, res)
-			break;
-		case 'exports':
-			onInitExportsController(req, res)
+		case 'service':
+			onInitServiceController(req, res)
 			break;
 		default:
 			res.status(404).json({
@@ -30,72 +28,33 @@ export const onInitController = (req: Request, res: Response) => {
 	}
 };
 
-export const onInitDomesticController = (req: Request, res: Response) => {
-	const {
-		context,
-		message: {
-			order: { provider, provider_location, ...order },
-		},
-	} = req.body;
-	const timestamp = (new Date()).toISOString();
-	const responseMessage = {
-		order: {
-			...order,
-			id: uuidv4(),
-			state: "Created",
-			provider: {
-				id: provider.id,
-				locations: [
-					{
-						...provider_location,
-					},
-				],
-			},
-			fulfillments: order.fulfillments.map(
-				({ id, type, tracking, stops }: any) => ({
-					id,
-					type,
-					tracking,
-					stops,
-				})
-			),
-			payments: [
-				{
-					params: {
-						...order.quote.price,
-					},
-					...order.payments[0],
-				},
-			],
-			created_at: timestamp,
-			updated_at: timestamp
-		},
-	};
+const onInitConsultationController = (req: Request, res: Response) => {
+	const { context } = req.body;
+	const file = fs.readFileSync(
+		path.join(SERVICES_EXAMPLES_PATH, "confirm/confirm_consultation.yaml")
+	);
+	const response = YAML.parse(file.toString());
 	return responseBuilder(
 		res,
 		context,
-		responseMessage,
+		response.value.message,
 		`${context.bap_uri}/${ACTIONS.confirm}`,
-		ACTIONS.confirm
-	);
-};
-
-export const onInitDomesticNonRfqController = (req: Request, res: Response) => {
-	return responseBuilder(
-		res,
-		req.body.context,
-		confirmNonRFQ.message,
-		req.body.context.bpp_uri,
 		`${ACTIONS.confirm}`
 	);
 };
 
-export const onInitExportsController = (req: Request, res: Response) => {
+const onInitServiceController = (req: Request, res: Response) => {
+	const { context } = req.body;
+	const file = fs.readFileSync(
+		path.join(SERVICES_EXAMPLES_PATH, "confirm/confirm_service.yaml")
+	);
+	const response = YAML.parse(file.toString());
 	return responseBuilder(
 		res,
-		req.body.context,
-		confirmExports.message,
-		req.body.context.bpp_uri,
+		context,
+		response.value.message,
+		`${context.bap_uri}/${ACTIONS.confirm}`,
 		`${ACTIONS.confirm}`
 	);
 };
+
