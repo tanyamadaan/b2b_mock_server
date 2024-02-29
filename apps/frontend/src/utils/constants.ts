57c458842ff0b8154640af7cf789f8faa23c8efc
Abhik Banerjee <106070541+abhik-wil@users.noreply.github.com>
2024-02-29 10:17:14 +0530

    Update constants.ts
    Added Readme link to frontend

--- apps/frontend/src/utils/constants.ts
+++ apps/frontend/src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const SUPPORTED_DOMAINS = ["B2B", "SERVICES"];
 
-export const USER_GUIDE_LINK = ""; 
+export const USER_GUIDE_LINK = "https://github.com/tanyamadaan/b2b_mock_server/blob/feat-monorepo/README.md"; 
 
 export const SWAGGER_BUILD_LINK =
 	"https://raw.githubusercontent.com/abhik-wil/b2b_mock_server/feat-monorepo/apps/backend/src/openapi/build/swagger.yaml";
