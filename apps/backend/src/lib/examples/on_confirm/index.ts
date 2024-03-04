8d35fb8db49a2e9f743d6b9971fcf815b060ce29
Abhik Banerjee <abhik.banerjee@thewitslab.com>
2024-03-04 13:28:39 +0530

    Started AJV porting to keywords and abstraction through wrapper

--- apps/backend/src/lib/examples/on_confirm/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./on_confirm_domestic"
-export * from "./on_confirm_domestic_non_rfq"
-export * from "./on_confirm_domestic_rejected"
-export * from "./on_confirm_exports"
--- apps/backend/src/lib/schema/b2b/index.ts
+++ apps/backend/src/lib/schema/b2b/index.ts
@@ -1,12 +1,80 @@
-export * from "./on_confirm";
-export * from "./on_init";
-export * from "./on_search";
-export * from "./select";
-export * from "./on_select";
-export * from "./on_update";
-export * from "./search";
-export * from "./init";
-export * from "./confirm";
-export * from "./status";
-export * from "./update";
-export * from "./on_status";
+import { NextFunction, Request, Response } from "express";
+import Ajv, { ValidateFunction } from "ajv";
+import addFormats from "ajv-formats";
+import { searchSchema } from "./search";
+
+export const b2bSchemaValidator =
+	(
+		action:
+			| "search"
+			| "on_search"
+			| "select"
+			| "on_select"
+			| "init"
+			| "on_init"
+			| "confirm"
+			| "on_confirm"
+			| "status"
+			| "on_status"
+			| "update"
+			| "on_update"
+			| "track"
+			| "on_track"
+			| "cancel"
+			| "on_cancel"
+	) =>
+	(req: Request, res: Response, next: NextFunction) => {
+		const ajv = new Ajv({
+			allErrors: true,
+			strict: false,
+			strictRequired: false,
+			strictTypes: false,
+			$data: true,
+		});
+		addFormats(ajv);
+
+		require("ajv-errors")(ajv);
+		var validate: ValidateFunction<{
+				[x: string]: {};
+			}>,
+			isValid: boolean;
+
+		switch (action) {
+			case "search":
+				validate = ajv.compile(searchSchema);
+				break;
+
+			default:
+				res.status(400).json({
+					message: {
+						ack: {
+							status: "NACK",
+						},
+					},
+					error: {
+						type: "JSON-SCHEMA-ERROR",
+						code: "50009",
+					},
+				});
+				return;
+		}
+
+		isValid = validate(req.body);
+
+		if (!isValid) {
+			res.status(400).json({
+				message: {
+					ack: {
+						status: "NACK",
+					},
+				},
+				error: {
+					type: "JSON-SCHEMA-ERROR",
+					code: "50009",
+					message: validate.errors?.map(({ message }) => ({ message })),
+				},
+			});
+			return;
+		}
+		next();
+	};
