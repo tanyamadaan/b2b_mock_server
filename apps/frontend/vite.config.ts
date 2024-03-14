c92a876aa1fcbe85066b070315e328234590b216
Abhik Banerjee <abhik.banerjee@thewitslab.com>
2024-03-14 14:17:25 +0530

    Fixes for NGINX.

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -6,4 +6,12 @@ import ViteYaml from '@modyfi/vite-plugin-yaml';
 export default defineConfig({
   plugins: [react(),
   ViteYaml()],
+  server: {
+    watch: {
+        usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+}
 })
