5ff45e310f0b0a491bb7a07a59b8c95e10c8cc5b
mayur.popli <mayur.popli@thewitslab.com>
2024-04-30 17:30:09 +0530

    fix : cancel fixes in services

--- apps/backend/src/controllers/services/bpp/cancel.ts
+++ apps/backend/src/controllers/services/bpp/cancel.ts
@@ -1,226 +1,226 @@
-// import { Request, Response } from "express";
-// import { SERVICES_EXAMPLES_PATH, responseBuilder } from "../../../lib/utils";
-// import fs from "fs";
-// import path from "path";
-// import YAML from "yaml";
-
-
-// export const cancelController = (req: Request, res: Response) => {
-// 	const { scenario } = req.query;
-// 	switch (scenario) {
-// 		case "ack":
-// 			cancelAckController(req, res);
-// 			break;
-// 		case "merchant":
-// 			cancelMerchantController(req, res);
-// 			break;
-// 		default:
-// 			res.status(404).json({
-// 				message: {
-// 					ack: {
-// 						status: "NACK",
-// 					},
-// 				},
-// 				error: {
-// 					message: "Invalid scenario",
-// 				},
-// 			});
-// 			break;
-// 	}
-// };
-
-
-// export const cancelAckController = (req: Request, res: Response) => {
-// 	const { context } = req.body;
-// 	const file = fs.readFileSync(
-// 		path.join(SERVICES_EXAMPLES_PATH, "on_cancel/on_cancel_ack.yaml")
-// 	);
-// 	const response = YAML.parse(file.toString());
-// 	return responseBuilder(
-// 		res,
-// 		context,
-// 		response.value.message,
-// 		`${context.bap_uri}/on_confirm`,
-// 		`on_confirm`,
-// 		"services"
-// 	);
-// };
-
-
-// export const cancelMerchantController = (req: Request, res: Response) => {
-// 	const { context } = req.body;
-// 	const file = fs.readFileSync(
-// 		path.join(SERVICES_EXAMPLES_PATH, "on_cancel/on_cancel_merchant.yaml")
-// 	);
-// 	const response = YAML.parse(file.toString());
-// 	return responseBuilder(
-// 		res,
-// 		context,
-// 		response.value.message,
-// 		`${context.bap_uri}/on_confirm`,
-// 		`on_confirm`,
-// 		"services"
-// 	);
-// };
 import { Request, Response } from "express";
-import { responseBuilder, B2B_EXAMPLES_PATH, redis } from "../../../lib/utils";
+import { SERVICES_EXAMPLES_PATH, responseBuilder } from "../../../lib/utils";
+import fs from "fs";
+import path from "path";
+import YAML from "yaml";
 
-export const cancelController = async (req: Request, res: Response) => {
+
+export const cancelController = (req: Request, res: Response) => {
 	const { scenario } = req.query;
-	const { transaction_id } = req.body.context;
-	const transactionKeys = await redis.keys(`${transaction_id}-*`);
-	const ifTransactionExist = transactionKeys.filter((e) =>
-		e.includes("on_confirm-from-server")
-	);
-
-	if (ifTransactionExist.length === 0) {
-		return res.status(400).json({
-			message: {
-				ack: {
-					status: "NACK",
+	switch (scenario) {
+		case "ack":
+			cancelAckController(req, res);
+			break;
+		case "merchant":
+			cancelMerchantController(req, res);
+			break;
+		default:
+			res.status(404).json({
+				message: {
+					ack: {
+						status: "NACK",
+					},
 				},
-			},
-			error: {
-				message: "on confirm doesn't exist",
-			},
-		});
-	}
-	const transaction = await redis.mget(ifTransactionExist);
-	const parsedTransaction = transaction.map((ele:any) => {
-		return JSON.parse(ele as string);
-	});
-	// getting on_search data for payment_ids
-	const search = await redis.mget(`${transaction_id}-on_search-from-server`);
-	const parsedSearch = search.map((ele:any) => {
-		return JSON.parse(ele as string);
-	})
-	// console.log("Search ::", parsedSearch[0].request.message.catalog.providers)
-
-	const provider_id = parsedTransaction[0].request.message.order.provider.id
-
-	const item_payment_ids = parsedSearch[0].request.message.catalog.providers.map((itm: any) => {
-		if (itm.id === provider_id) {
-			const result = itm.items.reduce((accumulator: any, currentItem: any) => {
-				accumulator[currentItem.id] = currentItem.payment_ids;
-				return accumulator;
-			}, {});
-			return result
-		}
-	})
-
-	if (parsedTransaction[0].request.message.order.id != req.body.message.order_id) {
-		return res.status(400).json({
-			message: {
-				ack: {
-					status: "NACK",
+				error: {
+					message: "Invalid scenario",
 				},
-			},
-			error: {
-				message: "Order id does not exist",
-			},
-		});
+			});
+			break;
 	}
+};
 
-	// console.log("Items with there ids :", item_payment_ids[0])
-	cancelRequest(req, res, parsedTransaction[0].request, item_payment_ids[0], scenario);
-}
 
-const cancelRequest = async (req: Request, res: Response, transaction: any, item_payment_ids: any, scenario: any) => {
-
+export const cancelAckController = (req: Request, res: Response) => {
 	const { context } = req.body;
-	
-	const responseMessage = {
-		...transaction.message,
-		order: {
-			...transaction.message.order,
-			state: "Cancelled",
-			cancellation: {
-				cancelled_by: req.body.context.bap_id,
-				reason: {
-					id: req.body.message.cancellation_reason_id
-				}
-			},
-			fulfillments: transaction.message.order.fulfillments.map((fulfillment: any) => ({
-				...fulfillment,
-				state: {
-
-					...fulfillment.state,
-					descriptor: {
-						code: "Cancelled"
-					}
-				},
-				stops: fulfillment.stops.map((stop: any) => {
-					// Add the instructions to both start and end stops
-					const instructions = {
-						name: "Proof of pickup",
-						short_desc: "Proof of pickup details",
-						long_desc: "Proof of pickup details",
-						images: [
-							"https://image1_url.png"
-						]
-					};
-					// Check if the stop type is "end"
-					if (stop.type === "end") {
-						// Add the agent object to the stop
-						return {
-							...stop,
-							instructions: {
-								...instructions,
-								name: "Proof of delivery",
-								short_desc: "Proof of delivery details",
-								long_desc: "Proof of delivery details",
-							},
-							location: {
-								...stop.location
-							},
-							agent: {
-								person: {
-									name: "Ramu"
-								},
-								contact: {
-									phone: "9886098860"
-								}
-							}
-						};
-					} else if (stop.type === "start") {
-						// For stops of type "start", add the instructions and location modifications
-						return {
-							...stop,
-							instructions,
-							location: {
-								...stop.location,
-								descriptor: {
-									...stop.location.descriptor,
-									images: ["https://gf-integration/images/5.png"]
-								}
-							}
-						};
-					} else {
-						// For other types, return the stop as is with instructions
-						return {
-							...stop,
-							instructions
-						};
-					}
-				}),
-				rateable: undefined
-			})),
-			items: transaction.message.order.items.map((itm: any) => ({
-				...itm,
-				payment_ids: item_payment_ids && item_payment_ids[itm.id] ? item_payment_ids[itm.id] : undefined
-			})),
-		}
-	}
-
+	const file = fs.readFileSync(
+		path.join(SERVICES_EXAMPLES_PATH, "on_cancel/on_cancel_ack.yaml")
+	);
+	const response = YAML.parse(file.toString());
 	return responseBuilder(
 		res,
 		context,
-		responseMessage,
-		`${req.body.context.bap_uri}${req.body.context.bap_uri.endsWith("/") ? "on_cancel" : "/on_cancel"
-		}`,
-		`on_cancel`,
-		"b2b",
-	)
-}
+		response.value.message,
+		`${context.bap_uri}/on_confirm`,
+		`on_confirm`,
+		"services"
+	);
+};
+
+
+export const cancelMerchantController = (req: Request, res: Response) => {
+	const { context } = req.body;
+	const file = fs.readFileSync(
+		path.join(SERVICES_EXAMPLES_PATH, "on_cancel/on_cancel_merchant.yaml")
+	);
+	const response = YAML.parse(file.toString());
+	return responseBuilder(
+		res,
+		context,
+		response.value.message,
+		`${context.bap_uri}/on_confirm`,
+		`on_confirm`,
+		"services"
+	);
+};
+// import { Request, Response } from "express";
+// import { responseBuilder, B2B_EXAMPLES_PATH, redis } from "../../../lib/utils";
+
+// export const cancelController = async (req: Request, res: Response) => {
+// 	const { scenario } = req.query;
+// 	const { transaction_id } = req.body.context;
+// 	const transactionKeys = await redis.keys(`${transaction_id}-*`);
+// 	const ifTransactionExist = transactionKeys.filter((e) =>
+// 		e.includes("on_confirm-from-server")
+// 	);
+
+// 	if (ifTransactionExist.length === 0) {
+// 		return res.status(400).json({
+// 			message: {
+// 				ack: {
+// 					status: "NACK",
+// 				},
+// 			},
+// 			error: {
+// 				message: "on confirm doesn't exist",
+// 			},
+// 		});
+// 	}
+// 	const transaction = await redis.mget(ifTransactionExist);
+// 	const parsedTransaction = transaction.map((ele:any) => {
+// 		return JSON.parse(ele as string);
+// 	});
+// 	// getting on_search data for payment_ids
+// 	const search = await redis.mget(`${transaction_id}-on_search-from-server`);
+// 	const parsedSearch = search.map((ele:any) => {
+// 		return JSON.parse(ele as string);
+// 	})
+// 	// console.log("Search ::", parsedSearch[0].request.message.catalog.providers)
+
+// 	const provider_id = parsedTransaction[0].request.message.order.provider.id
+
+// 	const item_payment_ids = parsedSearch[0].request.message.catalog.providers.map((itm: any) => {
+// 		if (itm.id === provider_id) {
+// 			const result = itm.items.reduce((accumulator: any, currentItem: any) => {
+// 				accumulator[currentItem.id] = currentItem.payment_ids;
+// 				return accumulator;
+// 			}, {});
+// 			return result
+// 		}
+// 	})
+
+// 	if (parsedTransaction[0].request.message.order.id != req.body.message.order_id) {
+// 		return res.status(400).json({
+// 			message: {
+// 				ack: {
+// 					status: "NACK",
+// 				},
+// 			},
+// 			error: {
+// 				message: "Order id does not exist",
+// 			},
+// 		});
+// 	}
+
+// 	// console.log("Items with there ids :", item_payment_ids[0])
+// 	cancelRequest(req, res, parsedTransaction[0].request, item_payment_ids[0], scenario);
+// }
+
+// const cancelRequest = async (req: Request, res: Response, transaction: any, item_payment_ids: any, scenario: any) => {
+
+// 	const { context } = req.body;
+	
+// 	const responseMessage = {
+// 		...transaction.message,
+// 		order: {
+// 			...transaction.message.order,
+// 			state: "Cancelled",
+// 			cancellation: {
+// 				cancelled_by: req.body.context.bap_id,
+// 				reason: {
+// 					id: req.body.message.cancellation_reason_id
+// 				}
+// 			},
+// 			fulfillments: transaction.message.order.fulfillments.map((fulfillment: any) => ({
+// 				...fulfillment,
+// 				state: {
+
+// 					...fulfillment.state,
+// 					descriptor: {
+// 						code: "Cancelled"
+// 					}
+// 				},
+// 				// stops: fulfillment.stops.map((stop: any) => {
+// 				// 	// Add the instructions to both start and end stops
+// 				// 	const instructions = {
+// 				// 		name: "Proof of pickup",
+// 				// 		short_desc: "Proof of pickup details",
+// 				// 		long_desc: "Proof of pickup details",
+// 				// 		images: [
+// 				// 			"https://image1_url.png"
+// 				// 		]
+// 				// 	};
+// 				// 	// Check if the stop type is "end"
+// 				// 	if (stop.type === "end") {
+// 				// 		// Add the agent object to the stop
+// 				// 		return {
+// 				// 			...stop,
+// 				// 			instructions: {
+// 				// 				...instructions,
+// 				// 				name: "Proof of delivery",
+// 				// 				short_desc: "Proof of delivery details",
+// 				// 				long_desc: "Proof of delivery details",
+// 				// 			},
+// 				// 			location: {
+// 				// 				...stop.location
+// 				// 			},
+// 				// 			agent: {
+// 				// 				person: {
+// 				// 					name: "Ramu"
+// 				// 				},
+// 				// 				contact: {
+// 				// 					phone: "9886098860"
+// 				// 				}
+// 				// 			}
+// 						};
+// 					} else if (stop.type === "start") {
+// 						// For stops of type "start", add the instructions and location modifications
+// 						return {
+// 							...stop,
+// 							instructions,
+// 							location: {
+// 								...stop.location,
+// 								descriptor: {
+// 									...stop.location.descriptor,
+// 									images: ["https://gf-integration/images/5.png"]
+// 								}
+// 							}
+// 						};
+// 					} else {
+// 						// For other types, return the stop as is with instructions
+// 						return {
+// 							...stop,
+// 							instructions
+// 						};
+// 					}
+// 				}),
+// 				rateable: undefined
+// 			})),
+// 			items: transaction.message.order.items.map((itm: any) => ({
+// 				...itm,
+// 				payment_ids: item_payment_ids && item_payment_ids[itm.id] ? item_payment_ids[itm.id] : undefined
+// 			})),
+// 		}
+// 	}
+
+// 	return responseBuilder(
+// 		res,
+// 		context,
+// 		responseMessage,
+// 		`${req.body.context.bap_uri}${req.body.context.bap_uri.endsWith("/") ? "on_cancel" : "/on_cancel"
+// 		}`,
+// 		`on_cancel`,
+// 		"b2b",
+// 	)
+// }
 
 
