82fc069840456a11bd101b04a5fc3c25d2ebca58
MayurWitsLab <126069887+MayurWitsLab@users.noreply.github.com>
2024-05-02 17:17:48 +0530

    Update select.ts

--- apps/backend/src/controllers/services/initiate/select.ts
+++ apps/backend/src/controllers/services/initiate/select.ts
@@ -35,20 +35,6 @@ export const initiateSelectController = async (req: Request, res: Response) => {
 	// const parsedTransaction = transaction.map((ele) => {
 	// 	return JSON.parse(ele as string);
 	// });
-	const prev_call = await redisExist("on_search", transactionId)
-	if (!prev_call) {
-		return res.status(400).json({
-			message: {
-				ack: {
-					status: "NACK",
-				},
-			},
-			error: {
-				message: "On search doesn't exist",
-			},
-		});
-	}
-
 	const on_search = await redisFetch("on_search", transactionId)
 	if (!on_search) {
 		return res.status(400).json({
