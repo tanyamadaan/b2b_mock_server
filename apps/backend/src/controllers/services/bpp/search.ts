78fd9bc5224afed35326deb3121a2746af604f96
mayur.popli <mayur.popli@thewitslab.com>
2024-05-03 18:51:50 +0530

    fix:fixes in services start and end date added

--- apps/backend/src/controllers/services/bpp/search.ts
+++ apps/backend/src/controllers/services/bpp/search.ts
@@ -1,29 +1,34 @@
 import { Request, Response } from "express";
-import {
-	SERVICES_EXAMPLES_PATH,
-	responseBuilder,
-} from "../../../lib/utils";
+import { SERVICES_EXAMPLES_PATH, responseBuilder } from "../../../lib/utils";
 import fs from "fs";
 import path from "path";
 import YAML from "yaml";
 
 export const searchController = (req: Request, res: Response) => {
-	const { message: { intent } } = req.body
-	const id = intent?.category?.id
+  const {
+    message: { intent },
+  } = req.body;
+  // const id = intent?.category?.id
 
-	// uncomment for default selection of customization response
-	// const id = "SRV11-1041" 
-	const file = fs.readFileSync(
-		path.join(SERVICES_EXAMPLES_PATH, `on_search/${id === "SRV11-1041" ? "on_search_customized.yaml" : "on_search.yaml"}`)
-	);
-	const response = YAML.parse(file.toString());
-	return responseBuilder(
-		res,
-		req.body.context,
-		response.value.message,
-		`${req.body.context.bap_uri}${req.body.context.bap_uri.endsWith("/") ? "on_search" : "/on_search"
-		}`,
-		`on_search`,
-		"services"
-	);
+  // uncomment for default selection of customization response
+  const id = "SRV11-1041";
+  const file = fs.readFileSync(
+    path.join(
+      SERVICES_EXAMPLES_PATH,
+      `on_search/${
+        id === "SRV11-1041" ? "on_search_customized.yaml" : "on_search.yaml"
+      }`
+    )
+  );
+  const response = YAML.parse(file.toString());
+  return responseBuilder(
+    res,
+    req.body.context,
+    response.value.message,
+    `${req.body.context.bap_uri}${
+      req.body.context.bap_uri.endsWith("/") ? "on_search" : "/on_search"
+    }`,
+    `on_search`,
+    "services"
+  );
 };
