8c3f1621709fe9738c368c823a638767dc2df0c5
mayur.popli <mayur.popli@thewitslab.com>
2024-05-06 19:38:45 +0530

    fix date schedule issue

--- apps/backend/src/controllers/services/initiate/select.ts
+++ apps/backend/src/controllers/services/initiate/select.ts
@@ -56,14 +56,13 @@ export const initiateSelectController = async (
 			},
 		});
 	}
-  // selecting the senarios
-  let scenario = "selection";
-  if (checkIfCustomized(on_search.message.catalog.providers[0].items)) {
-    scenario = "customization";
-  }
+	// selecting the senarios
+	let scenario = "selection";
+	if (checkIfCustomized(on_search.message.catalog.providers[0].items)) {
+		scenario = "customization";
+	}
 
 	const items = on_search.message.catalog.providers[0]?.categories;
-	// console.log("+++++", items)
 	let child_ids;
 	if (items) {
 		const parent_id = items.find(
@@ -77,7 +76,6 @@ export const initiateSelectController = async (
 		}, []);
 	}
 	req.body.child_ids = child_ids;
-	// console.log("Child_ids::", child_ids)
 	return intializeRequest(req, res, next, on_search, scenario);
 };
 
@@ -97,208 +95,208 @@ const intializeRequest = async (
 	const { transaction_id } = context;
 	const { id, locations } = providers[0];
 	// const { id: item_id, parent_item_id, location_ids } = providers[0].items[0];
-  let items = [];
-  let start;
-  let endDate;
-  if (scenario === "customization") {
-    const startDate = new Date(providers?.[0]?.time?.range?.start);
+	let items = [];
+	let start;
+	let endDate;
+	if (scenario === "customization") {
+		const startDate = new Date(providers?.[0]?.time?.range?.start);
 
-    const startCategory = providers?.[0]?.categories?.find((cat: any) => {
-      return cat.id === req.body.child_ids[0];
-    });
-    const startSchedule = startCategory?.tags?.find(
-      (ele: any) => ele.descriptor.code === "schedule"
-    );
-    const startTime = startSchedule?.list?.find(
-      (ele: any) => ele.descriptor.code === "start_time"
-    ).value;
+		const startCategory = providers?.[0]?.categories?.find((cat: any) => {
+			return cat.id === req.body.child_ids[0];
+		});
+		const startSchedule = startCategory?.tags?.find(
+			(ele: any) => ele.descriptor.code === "schedule"
+		);
+		const startTime = startSchedule?.list?.find(
+			(ele: any) => ele.descriptor.code === "start_time"
+		).value;
+		const hour = Number(startTime?.split(":")[0]);
+		const minutes = Number(startTime?.split(":")[1]);
 
-    start = new Date(startDate.setHours(Number(startTime?.split(":")[0])));
-    start = new Date(start.setMinutes(Number(startTime?.split(":")[1])));
-    // Checking for a valid date and time
-    const currentDate = new Date();
+		start = new Date(startDate);
+		start.setUTCHours(hour, minutes, 0, 0);
 
-    // Compare the start date with the current date and time
-    if (isBefore(start, currentDate)) {
-      currentDate.setHours(start.getHours());
-      currentDate.setMinutes(start.getMinutes());
-      currentDate.setSeconds(start.getSeconds());
-      start = addDays(currentDate, 1);
-    }
+		const currentDate = new Date();
 
-    const scheduleobj = providers[0]?.categories
-      .find((itm: any) => itm.id === req.body.child_ids[0])
-      ?.tags.find((tag: any) => tag.descriptor.code === "schedule");
+		// Compare the start date with the current date and time
+		if (isBefore(start, currentDate)) {
+			currentDate.setUTCHours(start.getUTCHours());
+			currentDate.setUTCMinutes(start.getUTCMinutes());
+			currentDate.setUTCSeconds(start.getUTCSeconds());
+			start = addDays(currentDate, 1);
+		}
 
-    const endDateFrequency = scheduleobj?.list.find(
-      (ele: any) => ele.descriptor.code === "frequency"
-    )?.value;
+		const scheduleobj = providers[0]?.categories
+			.find((itm: any) => itm.id === req.body.child_ids[0])
+			?.tags.find((tag: any) => tag.descriptor.code === "schedule");
 
-    const frequency = parseInt(endDateFrequency?.match(/\d+/)[0]);
+		const endDateFrequency = scheduleobj?.list.find(
+			(ele: any) => ele.descriptor.code === "frequency"
+		)?.value;
 
-    //end date
-    endDate = new Date(start.setHours(start.getHours() + frequency));
-  
-    //parent_item_id not in customization
-    items = [...providers[0].items];
-    // console.log("----------", req.body.child_ids)
-    if (req.body.child_ids) {
-      // items = items.filter(item => item.category_ids.includes(req.body.child_ids[0])).slice(0,1);
-      const new_items: any[] = [];
-      let count = 0;
-      let index = 0;
-      while (index < items.length && count < 2) {
-        if (items[index].category_ids.includes(req.body.child_ids[0])) {
-          if (
-            new_items.length > 0 &&
-            new_items[0].parent_item_id !== items[index].parent_item_id
-          ) {
-            continue;
-          }
-          new_items.push(items[index]);
-          count++;
-        }
-        index++;
-      }
-      const parent_item = items.find(
-        (item: any) => item.id === new_items[0].parent_item_id
-      );
-      items = [parent_item, ...new_items];
-    }
-    const { id: item_id, parent_item_id, location_ids } = items[0];
-    // console.log("Items:::", items)
-    items = [
-      {
-        id: item_id,
-        parent_item_id,
-        location_ids,
-        quantity: {
-          selected: {
-            count: 1,
-          },
-        },
-      },
-      ...items.slice(1).map((item: any) => {
-        return {
-          // ...item,
-          id: item.id,
-          parent_item_id: item.parent_item_id,
-          quantity: {
-            selected: {
-              count: 1,
-            },
-          },
-          category_ids: item.category_ids,
-          location_ids: [location_ids],
-          tags: item.tags.map((tag: any) => ({
-            ...tag,
-            list: tag.list.map((itm2: any, index: any) => {
-              if (index === 0) {
-                return {
-                  descriptor: {
-                    code: "type",
-                  },
-                  value: "customization",
-                };
-              } else {
-                return itm2; // Return the item unchanged if it's not the first element
-              }
-            }),
-          })),
-        };
-      }),
-    ];
-  } else {
-    items = providers[0].items = [
-      providers[0]?.items.map(
-        ({
-          id,
-          parent_item_id,
-          location_ids,
-        }: {
-          id: any;
-          parent_item_id: any;
-          location_ids: any;
-        }) => ({ id, parent_item_id, location_ids: [{ id: location_ids[0] }] })
-      )[0],
-    ];
-  }
-  // console.log("Items::", items, "Senario::", scenario)
-  const select = {
-    context: {
-      ...context,
-      timestamp: new Date().toISOString(),
-      action: "select",
-      bap_id: MOCKSERVER_ID,
-      bap_uri: SERVICES_BAP_MOCKSERVER_URL,
-      message_id: uuidv4(),
-    },
-    message: {
-      order: {
-        provider: {
-          id,
-          locations: [
-            {
-              id: locations[0]?.id,
-            },
-          ],
-        },
-        items: items.map((itm: any) => ({
-          ...itm,
-          location_ids: itm.location_ids
-            ? itm.location_ids.map((id: any) => String(id))
-            : undefined,
-          quantity: {
-            selected: {
-              count: 1,
-            },
-          },
-        })),
-        fulfillments: [
-          {
-            ...fulfillments[0],
-            type: fulfillments[0].type,
-            stops: [
-              {
-                type: "end",
-                location: {
-                  gps: "12.974002,77.613458",
-                  area_code: "560001",
-                },
-                time: {
-                  label: "selected",
-                  range: {
-                    // should be dynamic on the basis of scehdule
-                    start:
-                      providers[0]?.time?.schedule?.times?.[0] ?? new Date(),
-                    end: providers[0]?.time?.schedule?.times?.[1] ?? new Date(),
-                  },
-                },
-                days: scenario === "customization" ? "4" : undefined,
-                // 	? fulfillments[0].stops[0].time.days.split(",")[0]
-                // 	: undefined,
-              },
-            ],
-          },
-        ],
-        payments: [{ type: payments[0].type }],
-      },
-    },
-  };
-  if (eq(scenario, "customization")) {
-    set(
-      select,
-      "message.order.fulfillments[0].stops[0].time.range.start",
-      start
-    );
-    set(
-      select,
-      "message.order.fulfillments[0].stops[0].time.range.end",
-      endDate
-    );
-  }
+		const frequency = parseInt(endDateFrequency?.match(/\d+/)[0]);
 
-	// console.log("Final __ Items::", select.message.order.items)
+		//end date
+		endDate = new Date(start);
+		endDate.setUTCHours(start.getUTCHours() + frequency);
+
+		//parent_item_id not in customization
+		items = [...providers[0].items];
+		if (req.body.child_ids) {
+			// items = items.filter(item => item.category_ids.includes(req.body.child_ids[0])).slice(0,1);
+			const new_items: any[] = [];
+			let count = 0;
+			let index = 0;
+			while (index < items.length && count < 2) {
+				if (items[index].category_ids.includes(req.body.child_ids[0])) {
+					if (
+						new_items.length > 0 &&
+						new_items[0].parent_item_id !== items[index].parent_item_id
+					) {
+						continue;
+					}
+					new_items.push(items[index]);
+					count++;
+				}
+				index++;
+			}
+			const parent_item = items.find(
+				(item: any) => item.id === new_items[0].parent_item_id
+			);
+			items = [parent_item, ...new_items];
+		}
+		const { id: item_id, parent_item_id, location_ids } = items[0];
+		items = [
+			{
+				id: item_id,
+				parent_item_id,
+				location_ids,
+				quantity: {
+					selected: {
+						count: 1,
+					},
+				},
+			},
+			...items.slice(1).map((item: any) => {
+				return {
+					// ...item,
+					id: item.id,
+					parent_item_id: item.parent_item_id,
+					quantity: {
+						selected: {
+							count: 1,
+						},
+					},
+					category_ids: item.category_ids,
+					location_ids: [location_ids],
+					tags: item.tags.map((tag: any) => ({
+						...tag,
+						list: tag.list.map((itm2: any, index: any) => {
+							if (index === 0) {
+								return {
+									descriptor: {
+										code: "type",
+									},
+									value: "customization",
+								};
+							} else {
+								return itm2; // Return the item unchanged if it's not the first element
+							}
+						}),
+					})),
+				};
+			}),
+		];
+	} else {
+		items = providers[0].items = [
+			providers[0]?.items.map(
+				({
+					id,
+					parent_item_id,
+					location_ids,
+				}: {
+					id: any;
+					parent_item_id: any;
+					location_ids: any;
+				}) => ({ id, parent_item_id, location_ids: [{ id: location_ids[0] }] })
+			)[0],
+		];
+	}
+	// console.log("Items::", items, "Senario::", scenario)
+	const select = {
+		context: {
+			...context,
+			timestamp: new Date().toISOString(),
+			action: "select",
+			bap_id: MOCKSERVER_ID,
+			bap_uri: SERVICES_BAP_MOCKSERVER_URL,
+			message_id: uuidv4(),
+		},
+		message: {
+			order: {
+				provider: {
+					id,
+					locations: [
+						{
+							id: locations[0]?.id,
+						},
+					],
+				},
+				items: items.map((itm: any) => ({
+					...itm,
+					location_ids: itm.location_ids
+						? itm.location_ids.map((id: any) => String(id))
+						: undefined,
+					quantity: {
+						selected: {
+							count: 1,
+						},
+					},
+				})),
+				fulfillments: [
+					{
+						...fulfillments[0],
+						type: fulfillments[0].type,
+						stops: [
+							{
+								type: "end",
+								location: {
+									gps: "12.974002,77.613458",
+									area_code: "560001",
+								},
+								time: {
+									label: "selected",
+									range: {
+										// should be dynamic on the basis of scehdule
+										start:
+											providers[0]?.time?.schedule?.times?.[0] ?? new Date(),
+										end: providers[0]?.time?.schedule?.times?.[1] ?? new Date(),
+									},
+								},
+								days: scenario === "customization" ? "4" : undefined,
+								// 	? fulfillments[0].stops[0].time.days.split(",")[0]
+								// 	: undefined,
+							},
+						],
+					},
+				],
+				payments: [{ type: payments[0].type }],
+			},
+		},
+	};
+	if (eq(scenario, "customization")) {
+		set(
+			select,
+			"message.order.fulfillments[0].stops[0].time.range.start",
+			start
+		);
+		set(
+			select,
+			"message.order.fulfillments[0].stops[0].time.range.end",
+			endDate
+		);
+	}
+
 	const header = await createAuthHeader(select);
 	try {
 		await redis.set(
