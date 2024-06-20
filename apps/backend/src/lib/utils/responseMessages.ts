fb199aafbb534ce666e8d8ef8a9a4e12db4c0587
ParneetkaurDev <parneet.kaur@ondc.org>
2024-06-20 13:11:06 +0530

    cron code updated for agri and healthcare

--- apps/backend/src/lib/utils/responseMessages.ts
+++ apps/backend/src/lib/utils/responseMessages.ts
@@ -4,4 +4,5 @@ ORDER_ID_DOES_NOT_EXISTED:"Order id does not exist!",
 ON_CONFIRM_DOES_NOT_EXISTED:"on confirm doesn't exist",
 ON_SEARCH_DOES_NOT_EXISTED:"on search doesn't exist",
 ON_UPDATE_DOES_NOT_EXISTED:"on update doesn't exist",
+SELECTED_ITEMS_DOES_NOT_EXISTED:"selected items doesn't exist",
 }
