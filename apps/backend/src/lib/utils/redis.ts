f70d186ef08328665def7b8e0ae9a37eef873521
Abhik Banerjee <abhik.banerjee@thewitslab.com>
2024-08-07 12:58:59 +0530

    Commiting for working state.

--- apps/backend/src/lib/utils/redis.ts
+++ apps/backend/src/lib/utils/redis.ts
@@ -3,8 +3,8 @@ import Redis, { RedisOptions } from "ioredis";
 const redisOptions: RedisOptions = {
 	host: process.env.REDIS_HOST || "localhost",
 	port: (process.env.REDIS_PORT || 6379) as number,
-	username: process.env.REDIS_USER,
-	password: process.env.REDIS_PASS,
+	username: process.env.REDIS_USERNAME,
+	password: process.env.REDIS_PASSWORD,
 };
 
 export type TransactionType = {
