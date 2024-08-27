e45893c17ef60de1959eb3d732c0e05630e380c0
Abhik Banerjee <abhik.banerjee@thewitslab.com>
2024-08-27 10:29:59 +0530

    minor path issue fix

--- apps/backend/src/lib/utils/constants.ts
+++ apps/backend/src/lib/utils/constants.ts
@@ -16,7 +16,7 @@ export const BID_AUCTION_SERVICES_EXAMPLES_PATH =
 	"./domain-repos/@services/draft-agri_bids_and_auction/api/components/Examples/Agri_Bids_And_Auction_yaml";
 
 export const HEALTHCARE_SERVICES_EXAMPLES_PATH =
-	"./domain-repos/@services/draft-healthcare-service/api/components/Examples/Health_care_services_yaml";
+	"./domain-repos/@services/draft-healthcare-services/api/components/Examples/Health_care_services_yaml";
 
 export const AGRI_EQUIPMENT_HIRING_EXAMPLES_PATH =
 	"./domain-repos/@services/draft-agri_equipment/api/components/Examples/Agri_Equipment_Hiring_yaml";
