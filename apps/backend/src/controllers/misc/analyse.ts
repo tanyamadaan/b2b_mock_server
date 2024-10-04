2157c03dbc1609a40b36742dcf1c5f3db38948fd
mayur.popli <mayur.popli@thewitslab.com>
2024-10-04 13:37:56 +0530

    fix : b2b/b2c differenciation

--- apps/backend/src/controllers/misc/analyse.ts
+++ apps/backend/src/controllers/misc/analyse.ts
@@ -20,42 +20,61 @@ export const analyseController = async (req: Request, res: Response) => {
 
 	if (transactionKeys.length === 0) return res.json([]);
 
-	if (transactionKeys.filter((e) => e.match(/-from-server(?:-(\d+))?(?:-(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z))?$/) != null).length > 0) {
-    var _transactionsKeys = transactionKeys.filter((e) => e.match(/-from-server(?:-(\d+))?(?:-(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z))?$/));
-    var transactions = await redis.mget(_transactionsKeys);
-    storedTransaction.push(
-      transactions.map((each, index) => {
-        let _key = _transactionsKeys[index].match(/-from-server(?:-(\d+))?(?:-(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z))?$/);
-        if (!each) return null;
-        var parsed = JSON.parse(each);
-        return {
-          ...parsed,
-          id: _key ? _key[1] : '0',
-          type: "from_server",
-          action: (parsed.request as any).context.action,
-          timestamp: _key ? _key[2] : (parsed.request as any).context.timeStamp,
-        };
-      })
-    );
-  }
-  // if (transactionKeys.filter((e) => e.match(/-from-server-(\d+)$/) != null).length > 0) {
-  //   var _transactionsKeys = transactionKeys.filter((e) => e.match(/-from-server-(\d+)$/));
-  //   var transactions = await redis.mget(_transactionsKeys);
-  //   storedTransaction.push(
-  //     transactions.map((each, index) => {
-  //       let _key = _transactionsKeys[index].match(/-from-server-(\d+)$/);
-  //       if (!each) return null;
-  //       var parsed = JSON.parse(each);
-  //       return {
-  //         ...parsed,
-  //         id: _key ? _key[1] : '0',
-  //         type: "from_server",
-  //         action: (parsed.request as any).context.action,
-  //         timestamp: (parsed.request as any).context.timeStamp,
-  //       };
-  //     })
-  //   );
-  // }
+	if (
+		transactionKeys.filter(
+			(e) =>
+				e.match(
+					/-from-server(?:-(\d+))?(?:-(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z))?$/
+				) != null ||
+				e.match(
+					/-to-server(?:-(\d+))?(?:-(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z))?$/
+				) != null
+		).length > 0
+	) {
+		var _transactionsKeys = transactionKeys.filter(
+			(e) =>
+				e.match(
+					/-from-server(?:-(\d+))?(?:-(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z))?$/
+				) ||
+				e.match(
+					/-to-server(?:-(\d+))?(?:-(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z))?$/
+				)
+		);
+		var transactions = await redis.mget(_transactionsKeys);
+		storedTransaction.push(
+			transactions.map((each, index) => {
+				let _key = _transactionsKeys[index].match(
+					/-from-server(?:-(\d+))?(?:-(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z))?$/
+				);
+				if (!each) return null;
+				var parsed = JSON.parse(each);
+				return {
+					...parsed,
+					id: _key ? _key[1] : "0",
+					type: "from_server",
+					action: (parsed.request as any).context.action,
+					timestamp: _key ? _key[2] : (parsed.request as any).context.timeStamp,
+				};
+			})
+		);
+	}
+	// if (transactionKeys.filter((e) => e.match(/-from-server-(\d+)$/) != null).length > 0) {
+	//   var _transactionsKeys = transactionKeys.filter((e) => e.match(/-from-server-(\d+)$/));
+	//   var transactions = await redis.mget(_transactionsKeys);
+	//   storedTransaction.push(
+	//     transactions.map((each, index) => {
+	//       let _key = _transactionsKeys[index].match(/-from-server-(\d+)$/);
+	//       if (!each) return null;
+	//       var parsed = JSON.parse(each);
+	//       return {
+	//         ...parsed,
+	//         id: _key ? _key[1] : '0',
+	//         type: "from_server",
+	//         action: (parsed.request as any).context.action,
+	//         timestamp: (parsed.request as any).context.timeStamp,
+	//       };
+	//     })
+	//   );
+	// }
 	return res.json(storedTransaction.flat());
 };
-
